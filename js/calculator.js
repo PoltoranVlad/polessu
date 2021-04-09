fab3d3253f0f66a3fe55fe2957a9c7ec5958ea59
vlad <poltor.vlad@mail.ru>
2021-04-09 19:05:05 +0300

    first commit

---
+++ js/calculator.js
@@ -0,0 +1,33 @@
+const squareInput = document.querySelector('.square-input'),
+    resultText = document.querySelector('.price');
+let status;
+
+function calcResult(){
+    if(status === 'simple'){
+        resultText.textContent = 1.5 * squareInput.value;
+    }else{
+        resultText.textContent = 2 * squareInput.value;
+    }
+}
+
+function getStaticData(parentSelector, calcActive){
+    document.querySelector(parentSelector).addEventListener('click', event =>{
+        status = event.target.getAttribute('id');
+
+
+        const elements = document.querySelectorAll('.type')
+        elements.forEach(elem=>{
+            elem.classList.remove(calcActive)
+        })
+        event.target.classList.add(calcActive)
+
+        calcResult();
+    });
+}
+
+
+
+getStaticData('.select-calc', 'active')
+
+
+
